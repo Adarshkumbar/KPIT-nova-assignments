9717f8e266456664feac097c78b42717215066ea
Adarsh-Kumbar-in <adarshkumbarbgm@gmail.com>
2023-03-02 03:34:56 +0530

    finalised 3rd assignment

--- Assignment-3/bitmap.cpp
+++ Assignment-3/bitmap.cpp
@@ -5,34 +5,31 @@ using namespace std;
 
 Account::Account()
 {
-	// code here
+	//This is Default Constructor
 }
-
 
 Account::Account(int acc, const char *n, double bal)
 {
-	// code here
+	accno=acc,strcpy(name,n),balance=bal;
 }
-
 void Account::accept()
 {
-	// code here
+	cout<<"Enter Account Number,Name And Balance respectively:";
+	cin>>accno>>name>>balance;
 }
-
-
 void Account::display()
 {
-	// code here
+	cout<<"Account number:"<<accno<<"\n"<<"Name:"<<name<<"\n"<<"Balance:"<<balance;
 }
 
 void Account::deposite(int amount)
 {
-	// code here
+	balance=balance+amount;
 }
 
 void Account::withdraw(int amount)
 {
-	// code here
+	balance=balance-amount;
 }
 
 int Account::getAccno()
@@ -69,41 +66,35 @@ bool Account::searchAccountById(Account *accounts, int size, int id)
 {
 	bool status=false;
 	
-	// code here
-	
+	for(int i=0;i<size;i++)
+	{
+		if(id==accounts[i].getAccno())
+		{
+			status=true;
+			break;
+		}
+	}
 	return status;
 }
 
 double Account::getHighestBalance(Account *accounts, int size)
 {
-	int i, highestBal;
-	
-	// code here
-	
+	int i=0, highestBal=int(accounts[0].getBalance());
+	for(i=0;i<size;i++)
+	{
+		if(highestBal<accounts[i].balance)
+			highestBal=accounts[i].balance;
+	}
 	return highestBal;
 }
 
-
 double Account::getLowestBalance(Account *accounts, int size)
 {
-	int i, lowestBal;
-	
-	// code here
-		
+	int i, lowestBal=int(accounts[0].getBalance());
+	for(i=0;i<size;i++)
+	{
+		if(lowestBal<accounts[i].balance)
+			lowestBal=accounts[i].balance;
+	}	
 	return lowestBal;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
