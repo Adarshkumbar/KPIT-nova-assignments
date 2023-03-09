4fa67dc5ea034ade0294a5f504f8dd0c0a32452c
Adarsh-Kumbar-in <adarshkumbarbgm@gmail.com>
2023-03-09 18:08:08 +0530

    Update Final.cpp

--- Assignment-4/Final.cpp
+++ Assignment-4/Final.cpp
@@ -1,7 +1,65 @@
 #include <cstdlib>
 #include "bitmap.h"
+#define BITMAP_H
 
+#include<iostream>
 using namespace std;
+class Employee
+{
+private:
+
+    /*
+        class data members
+    */
+    std::string EmployeeId;
+    std::string EmployeeName;
+    float EmployeeSalary;
+    int PaidLeavesCount;
+    int PaidLeavesClaimed;
+
+public:
+
+    //default constructor
+    Employee() {}
+
+    //Parameterized constructor
+    Employee(std::string id, std::string name, float salary, int plCount, int plClaimed);
+    
+    /*
+        class method declaration
+    */
+    void ChangeName(std::string newName);
+
+    int CalculateLeavesReimbursement();
+
+    int GetPaidLeavesCount();
+    int GetClaimedPaidLeavesCount();
+    float GetEmployeeSalary();
+    std::string GetEmployeeName();
+    std::string GetEmployeeId();
+
+    ~Employee() {}
+};
+
+class Manager : public Employee
+{
+private:
+    int ProjectsCompleted;
+    std::string ManagerDepartment;
+public:
+
+    Manager (std::string id, std::string name, float salary, int plCount, 
+        int plClaimed, int projects,std::string department);
+
+    int CalculateBonus();
+
+    std::string GetManagerDepartment();
+    int GetProjectsCompletedCount();
+
+    ~Manager () {}
+};
+
+
 
 Employee::Employee(string id, string name, float salary, int plCount, int plClaimed) :
     EmployeeId(id), EmployeeName(name), EmployeeSalary(salary), PaidLeavesCount(plCount), 
